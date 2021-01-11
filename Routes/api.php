6d5ea92220868c706527b1fee7b807619fc982d0
Maxi <maxprimak1@gmail.com>
2021-01-11 11:39:26 +0100

    AuthController api added

--- Routes/api.php
+++ Routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
-use Modules\Accounts\Http\Controllers\AuthController\AuthController;
+use Modules\Accounts\Http\Controllers\Auth\AuthController;
 
 /*
 |--------------------------------------------------------------------------
