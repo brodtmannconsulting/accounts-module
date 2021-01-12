617a76bda9ad792c0159eb081da59d06b6069658
Maxi <maxprimak1@gmail.com>
2021-01-12 09:40:21 +0100

    AccountsController.php updated

--- Http/Controllers/AccountsController.php
+++ Http/Controllers/AccountsController.php
@@ -9,15 +9,6 @@ use Illuminate\Routing\Controller;
 class AccountsController extends Controller
 {
     /**
-     * Display a listing of the resource.
-     * @return Renderable
-     */
-    public function index()
-    {
-        return view('accounts::index');
-    }
-
-    /**
      * Show the form for creating a new resource.
      * @return Renderable
      */
