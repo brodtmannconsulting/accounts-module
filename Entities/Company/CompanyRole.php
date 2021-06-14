3cb6f427f690c4bb493448488c7fef8c7e644608
maxprimak <maxprimak1@gmail.com>
2021-06-14 17:50:08 +0200

    Delete RoleScopes when remove Role from company

--- Entities/Company/CompanyRole.php
+++ Entities/Company/CompanyRole.php
@@ -4,6 +4,7 @@ namespace Modules\Accounts\Entities\Company;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Modules\Accounts\Entities\Role\RoleScope;
 
 class CompanyRole extends Model
 {
@@ -15,4 +16,22 @@ class CompanyRole extends Model
     {
         return \Modules\Accounts\Database\factories\Company\CompanyRoleFactory::new();
     }
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::deleting(function (self $model)
+        {
+            $model->deleteRoleScopes();
+        });
+    }
+
+    private function deleteRoleScopes()
+    {
+        RoleScope::where('role_id', $this->attributes['role_id'])
+            ->where('company_id', $this->attributes['company_id'])
+            ->delete();
+    }
+
 }
