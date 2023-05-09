ed84a88aa33152b1249df3beb2613e851fe94647
James Terrono <jterrono@gmail.com>
2023-05-08 21:01:34 -0400

    remove passport routes

--- Providers/AuthServiceProvider.php
+++ Providers/AuthServiceProvider.php
@@ -130,9 +130,6 @@ class AuthServiceProvider extends ServiceProvider
 
         ]);
 
-
-        Passport::routes();
-
         Passport::tokensExpireIn(Carbon::now()->addDays(1));
 
         Passport::refreshTokensExpireIn(Carbon::now()->addDays(7));
